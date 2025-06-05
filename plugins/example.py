dc4b94f66e645b57067b031079ad36fc5c06ecca
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-06-05 20:32:49 +0200

    hotfix

--- plugins/example.py
+++ plugins/example.py
@@ -1,4 +1,9 @@
 def register(command_handlers, hooks, setup_functions=None, package_groups_extensions=None, custom_json_handlers=None):
     def my_command(args):
         print("example_command: Custom command executed!")
-    PLUGIN_COMMANDS.append(("example", my_command, "Example command description."))
+        
+    import sys
+    main_mod = sys.modules["__main__"]
+    plugin_commands = getattr(main_mod, "PLUGIN_COMMANDS", None)
+    if plugin_commands is not None:
+        plugin_commands.append(("example", my_command, "Example command description"))

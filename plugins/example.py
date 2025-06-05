c1c0c0c31d0e7343d499033e7848c3b41c8b3305
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-06-05 20:14:17 +0200

    Delete plugins/example.py
    uploaded wrong file

--- plugins/example.py
+++
@@ -1,5 +0,0 @@
-def register(command_handlers, hooks, setup_functions=None, package_groups_extensions=None, custom_json_handlers=None):
-    def my_setup():
-        print("Hello from example_setup plugin! This is a custom setup.")
-    if setup_functions is not None:
-        setup_functions["example-setup"] = my_setup

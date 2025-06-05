d65a7377ad113cc32823a2cdc334373a882c9e7a
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-06-05 20:25:59 +0200

    Update example.py

--- plugins/example.py
+++ plugins/example.py
@@ -1,4 +1,4 @@
 def register(command_handlers, hooks, setup_functions=None, package_groups_extensions=None, custom_json_handlers=None):
     def my_command(args):
         print("example_command: Custom command executed!")
-    command_handlers["example"] = my_command
+    PLUGIN_COMMANDS.append(("example", my_command, "Example command description."))

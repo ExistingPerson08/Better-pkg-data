49bfbb27993cc8e0b526486f51378b7fc4bd9693
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-06-05 20:12:36 +0200

    Delete plugins/example.py

--- plugins/example.py
+++
@@ -1,6 +0,0 @@
-# ~/.local/share/better-tools/plugins/example.py
-def register(command_handlers, hooks=None):
-    def my_pre_upgrade(args):
-        print("Plugin: I do something during upgrade!")
-    if hooks:
-        hooks["upgrade-plugin"].append(my_pre_upgrade)

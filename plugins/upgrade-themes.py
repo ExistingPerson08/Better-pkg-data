94c0e1bd5ac395aa3805a40470a5ffc99806e3a2
ExistingPerson <164225044+ExistingPerson08@users.noreply.github.com>
2025-06-06 16:13:07 +0200

    added colors

--- plugins/upgrade-themes.py
+++ plugins/upgrade-themes.py
@@ -3,6 +3,26 @@ def register(command_handlers, hooks, setup_functions=None, package_groups_exten
     import os
     import subprocess
 
+    class Colors:
+        def __init__(self):
+            self.use_color = "NO_COLOR" not in os.environ
+            if self.use_color:
+                self.NC = '\033[0m'
+                self.BGreen = '\033[1;32m'
+                self.BCyan = '\033[1;36m'
+                self.BYellow = '\033[1;33m'
+                self.BPurple = '\033[1;35m'
+                self.BRed = '\033[1;31m'
+                self.BWhite = '\033[1;37m'
+                self.c1 = '\u001b[38;5;104m'  # light purple
+                self.c2 = '\u001b[0m'         # white/reset
+                self.c3 = '\u001b[38;5;55m'   # dark purple
+                self.c4 = '\u001b[38;5;98m'   # medium purple
+            else:
+                self.NC = self.BGreen = self.BCyan = self.BYellow = ''
+                self.BPurple = self.BRed = self.BWhite = ''
+                self.c1 = self.c2 = self.c3 = self.c4 = ''
+
     def print_status(message, status="info"):
         """Barevný výpis stavových zpráv"""
         colors = Colors()
